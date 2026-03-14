defc4d7f0644f571f39449ff0b4bcd74f60329a1
Puneet-Ramini <ramini.p@northeastern.edu>
2026-03-14 11:32:16 -0400

    Initial Commit

---
+++ MBTA/MBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTAApp.swift
+//  MBTA
+//
+//  Created by Puneet Ramini on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct MBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
